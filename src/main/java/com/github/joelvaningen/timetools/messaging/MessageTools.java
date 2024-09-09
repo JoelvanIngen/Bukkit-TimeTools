6c094454383c1e3e0dfb875b61cba50cd97a7371
Joel <j.c.ingen@gmail.com>
2024-09-09 19:52:02 +0200

    Use MiniMessage for chat formatting

--- src/main/java/com/github/joelvaningen/timetools/messaging/MessageTools.java
+++ src/main/java/com/github/joelvaningen/timetools/messaging/MessageTools.java
@@ -1,12 +1,17 @@
 package com.github.joelvaningen.timetools.messaging;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 
 public class MessageTools {
     public static void messageAllPlayers(Server server, String message) {
+        MiniMessage miniMessage = MiniMessage.miniMessage();
+        Component parsed = miniMessage.deserialize(message);
+
         for (Player player : server.getOnlinePlayers()) {
-            player.sendMessage(message);
+            player.sendMessage(parsed);
         }
     }
 }
